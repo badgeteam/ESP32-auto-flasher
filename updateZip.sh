8f487bb1e03a275b1b4a435065dbb2fb12a2d6fa
Anne Jan Brouwer <brouwer@annejan.com>
2019-02-09 17:30:05 +0100

    maak zip

--- updateZip.sh
+++ updateZip.sh
@@ -5,12 +5,8 @@ set -e
 cd firmware/locfd
 
 for type in sl n f; do
-	cp media/badge_type_$type.png media/badge_type.png
-
 	# create with maximum compression to a temporary file.
 	zip -r -9 ../locfd-$type.zip.$$ . -x .ds_store
-
-	rm media/badge_type.png
 
 	# always do an atomic move.
 	mv ../locfd-$type.zip.$$ ../locfd-$type.zip
